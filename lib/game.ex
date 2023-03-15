ba8b601d9df0dc762a22db7d7f90aa3c6c413283
netto <euclydes.young@bluetab.net>
2023-03-16 00:28:48 +0100

    parsed game state

--- lib/game.ex
+++ lib/game.ex
@@ -10,4 +10,17 @@ defmodule Game do
   def stop(server), do: GameServer.stop(server)
 
   def view(server), do: GameServer.view(server)
+
+  def state(server) do
+    %{players: players, fruits: fruits} = GameServer.view(server)
+
+    fruits = Enum.map(fruits, fn {pos, {size, type}} -> %{pos: pos, size: size, type: type} end)
+
+    players =
+      Enum.map(players, fn {_node, player} ->
+        Map.take(player, [:name, :pos, :status, :status_timer, :score, :powered])
+      end)
+
+    %{players: players, fruits: fruits}
+  end
 end
